43e400b96c7bde8536662873129ace9ab8c93503
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-04-10 18:52:02 -0700

    Reformatted `CmakeLists.txt` files

--- bullet/UseBullet.cmake
+++ bullet/UseBullet.cmake
@@ -1,10 +1,8 @@
-#                                               -*- cmake -*-
+#-*- cmake -*-
 #
 #  UseBullet.cmake
 #
 
-
-add_definitions     ( ${BULLET_DEFINITIONS} )
-include_directories ( ${BULLET_INCLUDE_DIRS} )
-link_directories    ( ${BULLET_LIBRARY_DIRS} )
-
+add_definitions(${BULLET_DEFINITIONS})
+include_directories(${BULLET_INCLUDE_DIRS})
+link_directories(${BULLET_LIBRARY_DIRS})
